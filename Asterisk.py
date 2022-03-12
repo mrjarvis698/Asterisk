a8e6c7326299a95d2c8043c44610367963c2e378
mrjarvis698 <dudamajaykumar59@gmail.com>
2022-03-12 17:17:25 +0530

    main_window: hide Terminal Console while standalone launch
    Signed-off-by: mrjarvis698 <dudamajaykumar59@gmail.com>

--- Asterisk.py
+++ Asterisk.py
@@ -1,6 +1,8 @@
 from tkinter import *
+import win32gui, win32con
 
 def main():
+    win32gui.ShowWindow(win32gui.GetForegroundWindow(), win32con.SW_HIDE)
     main_window = Tk()
     Label(main_window, text="Hello World").pack()
     main_window.mainloop()
