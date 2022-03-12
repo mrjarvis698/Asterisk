e824e5adcd53de68385cf1b000c4213bd5445eae
mrjarvis698 <dudamajaykumar59@gmail.com>
2022-03-12 17:36:23 +0530

    Stop Hiding Console for now
    Signed-off-by: mrjarvis698 <dudamajaykumar59@gmail.com>

--- Asterisk.py
+++ Asterisk.py
@@ -2,7 +2,7 @@ from tkinter import *
 import win32gui, win32con
 
 def main():
-    win32gui.ShowWindow(win32gui.GetForegroundWindow(), win32con.SW_HIDE)
+    #win32gui.ShowWindow(win32gui.GetForegroundWindow(), win32con.SW_HIDE)
     main_window = Tk()
     Label(main_window, text="Hello World").pack()
     main_window.mainloop()
