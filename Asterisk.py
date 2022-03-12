89311d930613f0b216e97f718d77b28d37539de0
mrjarvis698 <dudamajaykumar59@gmail.com>
2022-03-12 17:44:10 +0530

    main_window: geometry: window size 	set non-maximized window to "1020x768" 	set maximized as default
    Signed-off-by: mrjarvis698 <dudamajaykumar59@gmail.com>

--- Asterisk.py
+++ Asterisk.py
@@ -4,6 +4,8 @@ import win32gui, win32con
 def main():
     #win32gui.ShowWindow(win32gui.GetForegroundWindow(), win32con.SW_HIDE)
     main_window = Tk()
+    main_window.geometry("1024x768")
+    main_window.state('zoomed')
     Label(main_window, text="Hello World").pack()
     main_window.mainloop()
 
